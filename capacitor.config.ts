64cd5e97c9b2b621392adc101ac2565a0d6b55d4
meshery1440 <55020916+meshery1440@users.noreply.github.com>
2025-08-11 03:42:14 +0300

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.mashai.app',
-  appName: 'Mash AI',
-  webDir: '.next'
-};
-
-export default config;
