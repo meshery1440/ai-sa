f4529bc81beca04539d24ebf8f4fc565bb1d90d3
meshery1440 <55020916+meshery1440@users.noreply.github.com>
2025-08-11 03:35:30 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mashai.app',
+  appName: 'Mash AI',
+  webDir: '.next'
+};
+
+export default config;
